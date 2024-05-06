e06993ba03dbdda9e43a8eb4fd7d5e041c8499ea
MichelleChinYeeLin <michellechinyl@gmail.com>
2024-05-06 12:44:41 +0800

    Add feedback page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import SignUp from "./pages/SignUpPage.tsx";
 import SignIn from "./pages/SignInPage.tsx";
 import UserProfilePage from "./pages/UserProfilePage.tsx";
 import UserHomePage from "./pages/UserHomePage.tsx";
+import UserFeedbackPage from "./pages/UserFeedbackPage.tsx";
 import UserTechnicalAssessmentQuestionPage from "./pages/UserTechnicalAssessmentQuestionPage.tsx";
 import UserAssessmentSubmissionCompletedPage from "./pages/UserAssessmentSubmissionCompletedPage.tsx";
 
@@ -20,6 +21,7 @@ function App() {
                 <Route path="/login" element={<SignIn/>}/>
                 <Route path="/profile" element={<UserProfilePage/>}/>
                 <Route path="/home" element={<UserHomePage/>}/>
+                <Route path="/feedback" element={<UserFeedbackPage/>}/>
                 <Route path="/technical" element={<UserTechnicalAssessmentQuestionPage/>}/>
                 <Route path="/submission-completed" element={<UserAssessmentSubmissionCompletedPage/>}/>
                 <Route path="/" element={<SignIn/>}/>
